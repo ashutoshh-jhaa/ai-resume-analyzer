e8721a857327c02f31478043174ddcec9099cc72
ash <ashutoshh.dev@gmail.com>
2025-09-02 11:28:26 +0530

    upload form

---
+++ app/lib/utils.ts
@@ -0,0 +1,15 @@
+//utility function to format size in different formats
+export const formatSize = (bytes: number): string => {
+  if (bytes === 0) return "0 Bytes";
+
+  const k = 1024;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+
+  // determine the appropriate unit
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  // divide by the unit and round to 2 decimals
+  const size = bytes / Math.pow(k, i);
+
+  return `${size.toFixed(2)} ${sizes[i]}`;
+};
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,7 +15,7 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+const Home = () => {
   const { auth } = usePuterStore();
   const navigate = useNavigate();
 
@@ -44,4 +44,5 @@ export default function Home() {
       </section>
     </main>
   );
-}
+};
+export default Home;
